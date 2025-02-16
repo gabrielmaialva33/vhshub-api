420bdc5f3d97008ce3b3986d25347537b86e717c
Gabriel Maia <gabrielmaialva33@gmail.com>
2025-02-15 22:51:32 -0300

    refactor: file path handling in knexfile.ts
    Replaced string concatenation with `path.join` for better readability and cross-platform compatibility. This ensures proper path handling across different operating systems, avoiding potential issues.

--- knexfile.ts
+++ knexfile.ts
@@ -1,20 +1,25 @@
 import 'dotenv/config';
+
+import * as path from 'path';
 import type { Knex } from 'knex';
 
 const config: { [key: string]: Knex.Config } = {
   development: {
     client: 'better-sqlite3',
     connection: {
-      filename: process.cwd() + '/src/database/' + process.env.DATABASE_URL,
+      filename: path.join(
+        process.cwd(),
+        'src',
+        'database',
+        process.env.DATABASE_URL,
+      ),
     },
     useNullAsDefault: true,
     migrations: {
-      directory: './src/database/migrations',
+      directory: path.join(process.cwd(), 'src', 'database', 'migrations'),
     },
     seeds: {
-      directory: './src/database/seeds',
+      directory: path.join(process.cwd(), 'src', 'database', 'seeds'),
     },
   },
 };
-
-module.exports = config;
