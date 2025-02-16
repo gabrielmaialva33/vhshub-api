49035f55ab37a4be200f0d54b83bf52e280fa93b
Isaque de Sousa <isaque.error404@gmail.com>
2025-02-16 18:32:45 -0300

    feat: create schema user

--- src/modules/users/dto/create-user.dto.ts
+++ src/modules/users/dto/create-user.dto.ts
@@ -1,1 +1,19 @@
-export class CreateUserDto {}
+import { db } from '@src/database';
+import { z } from 'zod';
+import { IUser } from '../interfaces/user.interface';
+
+export const createUserSchema = z.object({
+  name: z.string().min(1),
+  username: z
+    .string()
+    .min(1)
+    .refine(async (value) => {
+      const isExiting = await db<IUser>('users').where({ username: value });
+
+      if (isExiting.length > 0) return false;
+      return true;
+    }),
+  email: z.string().email('This is not a valid email'),
+  password: z.string().min(6, 'Password must be at least 6 characters long'),
+});
+export type CreateUserDto = z.infer<typeof createUserSchema>;
--- src/modules/users/dto/update-user.dto.ts
+++ src/modules/users/dto/update-user.dto.ts
@@ -1,4 +1,1 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateUserDto } from '@src/modules/users/dto/create-user.dto';
-
-export class UpdateUserDto extends PartialType(CreateUserDto) {}
+export class UpdateUserDto {}
