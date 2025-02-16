2a192747d6d90b685cef87ec50ee197520aadb8a
Isaque de Sousa <isaque.error404@gmail.com>
2025-02-16 18:33:09 -0300

    feat: add zod validation pipe

--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { UsersController } from '@src/modules/users/controllers/users.controller';
-import { UsersService } from '@src/modules/users/services/users.service';
 import { USERS_REPOSITORY } from '@src/modules/users/interfaces/user.interface';
 import { UsersRepository } from '@src/modules/users/repositories/users.repository';
+import { UsersService } from '@src/modules/users/services/users.service';
+import { ZodValidationPipe } from '@src/pipes/zod-validation.pipe';
 
 @Module({
   controllers: [UsersController],
@@ -12,6 +13,7 @@ import { UsersRepository } from '@src/modules/users/repositories/users.repositor
       provide: USERS_REPOSITORY,
       useClass: UsersRepository,
     },
+    ZodValidationPipe,
   ],
 })
 export class UsersModule {}
