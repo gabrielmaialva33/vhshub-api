404fba61cdcc839e50daf439a94500a382d3e531
Isaque de Sousa <isaque.error404@gmail.com>
2025-02-18 00:27:28 -0300

    feat: add user uniqueness check and password validation

--- src/modules/users/entities/user.entity.ts
+++ src/modules/users/entities/user.entity.ts
@@ -1,1 +1,16 @@
-export class User {}
+import { db } from '@src/database';
+import { IUser } from '../interfaces/user.interface';
+
+export class User {
+  static tableName = 'users';
+
+  static isUnique = async (field: keyof IUser, value: string) => {
+    const exiting = await db<IUser>(this.tableName)
+      .where({ [field]: value })
+      .first();
+    return !exiting;
+  };
+
+  static passwordRegex =
+    /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()_+~\-={}[\]:;"'<>,.?/|]).{8,}$/;
+}
