f01ea297e4967055bad379b7fd219bd88bf974c3
Isaque de Sousa <isaque.error404@gmail.com>
2025-02-18 00:28:10 -0300

    refactor: add entities

--- src/modules/users/dto/create-user.dto.ts
+++ src/modules/users/dto/create-user.dto.ts
@@ -1,29 +1,27 @@
-import { db } from '@src/database';
 import { z } from 'zod';
-import { IUser } from '../interfaces/user.interface';
-
-const isUnique = async (field: keyof IUser, value: string) => {
-  const exiting = await db<IUser>('users')
-    .where({ [field]: value })
-    .first();
-  return !exiting;
-};
+import { User } from '../entities/user.entity';
 
 export const createUserSchema = z.object({
   name: z.string().min(1),
   username: z
     .string()
     .min(1)
-    .refine(async (value) => isUnique('username', value), {
+    .refine(async (value) => User.isUnique('username', value), {
       message: 'Username already exists',
     }),
   email: z
     .string()
     .email('This is not a valid email')
-    .refine(async (value) => isUnique('email', value), {
+    .refine(async (value) => User.isUnique('email', value), {
       message: 'Email already exists',
     }),
-  password: z.string().min(6, 'Password must be at least 6 characters long'),
+  password: z
+    .string()
+    .min(6, 'Password must be at least 6 characters long')
+    .refine((value) => User.passwordRegex.test(value), {
+      message:
+        'Password must contain at least one uppercase letter, one lowercase letter, one number, and one special character.',
+    }),
 });
 
 export type CreateUserDto = z.infer<typeof createUserSchema>;
