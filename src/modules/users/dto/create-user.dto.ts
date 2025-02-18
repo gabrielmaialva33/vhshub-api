68eabcf0cc29c5413c9ac8b8cd4dcef74228736f
Isaque de Sousa <isaque.error404@gmail.com>
2025-02-17 23:24:50 -0300

    refactor: extract reusable function for unique field validation

--- src/modules/users/dto/create-user.dto.ts
+++ src/modules/users/dto/create-user.dto.ts
@@ -2,18 +2,28 @@ import { db } from '@src/database';
 import { z } from 'zod';
 import { IUser } from '../interfaces/user.interface';
 
+const isUnique = async (field: keyof IUser, value: string) => {
+  const exiting = await db<IUser>('users')
+    .where({ [field]: value })
+    .first();
+  return !exiting;
+};
+
 export const createUserSchema = z.object({
   name: z.string().min(1),
   username: z
     .string()
     .min(1)
-    .refine(async (value) => {
-      const isExiting = await db<IUser>('users').where({ username: value });
-
-      if (isExiting.length > 0) return false;
-      return true;
+    .refine(async (value) => isUnique('username', value), {
+      message: 'Username already exists',
     }),
-  email: z.string().email('This is not a valid email'),
+  email: z
+    .string()
+    .email('This is not a valid email')
+    .refine(async (value) => isUnique('email', value), {
+      message: 'Email already exists',
+    }),
   password: z.string().min(6, 'Password must be at least 6 characters long'),
 });
+
 export type CreateUserDto = z.infer<typeof createUserSchema>;
