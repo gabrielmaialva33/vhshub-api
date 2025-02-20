0e0aea839d9d3afa0e406581a6c295278b84fb15
Isaque de Sousa <isaque.error404@gmail.com>
2025-02-20 15:01:33 -0300

    refactor: add regex validation email

--- src/modules/users/entities/user.entity.ts
+++ src/modules/users/entities/user.entity.ts
@@ -11,6 +11,8 @@ export class User {
     return !exiting;
   };
 
+  static emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
   static passwordRegex =
     /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()_+~\-={}[\]:;"'<>,.?/|]).{8,}$/;
 }
