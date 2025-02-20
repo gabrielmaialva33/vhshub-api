7dfb0c51fae548537dc1fab5b80b769e0bcac567
Isaque de Sousa <isaque.error404@gmail.com>
2025-02-20 10:38:56 -0300

    feat: add validation zod

--- src/modules/users/dto/update-user.dto.ts
+++ src/modules/users/dto/update-user.dto.ts
@@ -1,1 +1,32 @@
-export class UpdateUserDto {}
+import { z } from 'zod';
+import { User } from '../entities/user.entity';
+export const updateUserSchema = z.object({
+  name: z.string().min(1).optional(),
+
+  username: z
+    .string()
+    .min(1)
+    .refine(async (value) => User.isUnique('username', value), {
+      message: 'Username already exists',
+    })
+    .optional(),
+
+  email: z
+    .string()
+    .email('This is not a valid email')
+    .refine(async (value) => User.isUnique('email', value), {
+      message: 'Email already exists',
+    })
+    .optional(),
+
+  password: z
+    .string()
+    .min(6, 'Password must be at least 6 characters long')
+    .refine((value) => User.passwordRegex.test(value), {
+      message:
+        'Password must contain at least one uppercase letter, one lowercase letter, one number, and one special character.',
+    })
+    .optional(),
+});
+
+export type UpdateUserDto = z.infer<typeof updateUserSchema>;
