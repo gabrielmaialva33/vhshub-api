f5930cf69d4ce44283cb017882333bfe40f454cd
Isaque de Sousa <isaque.error404@gmail.com>
2025-02-22 12:13:17 -0300

    chore: import interface

--- src/modules/users/services/users.service.spec.ts
+++ src/modules/users/services/users.service.spec.ts
@@ -1,8 +1,10 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { IUserRepository } from '../interfaces/user.interface';
 import { UsersService } from './users.service';
 
 describe('UsersService', () => {
   let service: UsersService;
+  let usersRepository: IUserRepository;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
