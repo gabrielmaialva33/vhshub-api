8c4bd8629b5e391c201f605c3591b6893f62e793
Gabriel Maia <gabrielmaialva33@gmail.com>
2025-02-22 12:57:12 -0300

    chore: add unit test for UsersService create method
    This commit introduces a test case verifying the `create` method in the `UsersService`. It mocks the repository, validates the method call, and checks the returned user object for correct structure and properties.

--- src/modules/users/services/users.service.spec.ts
+++ src/modules/users/services/users.service.spec.ts
@@ -7,10 +7,11 @@ import {
   USERS_REPOSITORY,
 } from '@src/modules/users/interfaces/user.interface';
 import { UsersService } from '@src/modules/users/services/users.service';
+import { CreateUserDto } from '@src/modules/users/dto/create-user.dto';
 
 describe('UsersService', () => {
   let service: UsersService;
-  let usersRepository: IUserRepository;
+  let usersRepository: jest.Mocked<IUserRepository>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -24,11 +25,41 @@ describe('UsersService', () => {
     }).compile();
 
     service = module.get<UsersService>(UsersService);
-    usersRepository = module.get<IUserRepository>(USERS_REPOSITORY);
+    usersRepository = module.get(USERS_REPOSITORY);
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
     expect(usersRepository).toBeDefined();
+  });
+
+  it('should call create method', async () => {
+    // todo - create a factory for CreateUserDto (stub) with https://fakerjs.dev/
+    const userData: CreateUserDto = {
+      name: 'John Doe',
+      email: 'jd@email.com',
+      password: '12345678',
+      username: 'johndoe',
+    };
+
+    // mock create method from repository
+    usersRepository.create.mockResolvedValue({
+      id: 1,
+      ...userData,
+      created_at: new Date(),
+      updated_at: new Date(),
+    });
+
+    // call service method
+    const createdUser = await service.create(userData);
+
+    // repository assertions
+    expect(usersRepository.create).toBeCalledWith(userData);
+    expect(usersRepository.create).toBeCalledTimes(1);
+
+    // service assertions
+    expect(createdUser).toBeDefined();
+    expect(createdUser.id).toBeDefined();
+    expect(createdUser.id).toEqual(expect.any(Number));
   });
 });
